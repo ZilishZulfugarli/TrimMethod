9b082d219b55e906a11e47104eb609eae2744fb4
ZilishZulfugarli <2002zilis@gmail.com>
2023-06-17 03:13:05 +0400

    Add project files.

---
+++ TrimMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace TrimMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = " *Azerbaijan  *";
+            b = TrimMethod.Trim(b);
+            Console.WriteLine(b);
+
+            Console.WriteLine("Enter:  ");
+            string c =Console.ReadLine();
+            c = TrimMethod.TrimStart(c);
+            Console.WriteLine(c);
+
+
+            string d = "***Hello World   * * * * ";
+            d = TrimMethod.TrimEnd(d);
+            Console.WriteLine(d);
+
+        }
+    }
+
+    static class TrimMethod
+    {
+        public static string Trim(string cumle)
+        {
+
+            char[] a = { ' ', '*' , ':' , ';' };
+            string temiz = cumle.Trim(a);
+            return temiz;
+           
+        }
+
+        public static string TrimStart(string cumle)
+        {
+            char[] a = { ' ', '+', '*', '/' };
+            string temiz = cumle.TrimStart(a);
+            return temiz;
+        }
+
+        public static string TrimEnd(string cumle)
+        {
+            char[] a = { ' ', '+', '*', '/' };
+            string temiz = cumle.TrimEnd(a);
+            return temiz;
+        }
+
+
+    }
+}
